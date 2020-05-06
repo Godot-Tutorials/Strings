4786a5ee27a05da9bb184106510b3620c6c3e1e7
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2020-05-05 19:41:48 -1000

    Add files via upload

---
+++ StringScript.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var stringValue = "String"
+var test = stringValue
+
+func _ready():
+	print("for loop print: ")
+	for character in stringValue:
+		print(character)
+	
+	var stringLength = stringValue.length()
+	var stringArray = stringValue.split('')
+	var stringInsert = stringValue.insert(0, 'New ')
+	
+	print("\nCharacter of string in first position", stringValue[0])
+	print("\nLength of String: ", stringLength)
+	print("\nArray item in position 0: ", stringArray[0])
+	print("\nNew String with inserted value: ", stringInsert)
